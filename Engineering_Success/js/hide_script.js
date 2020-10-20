ebfff0bdefb844e7558b33247a8763e9d5bec57d
ruchirgupta121 <32531253+ruchirgupta121@users.noreply.github.com>
2020-10-20 11:48:16 +0530

    updates

---
+++ Engineering_Success/js/hide_script.js
@@ -0,0 +1,25 @@
+function dispFunc1() {
+    var x = document.getElementById("prot");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+    }
+}
+    
+function dispFunc2() {    
+    var y = document.getElementById("trouble");
+        if (y.style.display === "none") {
+            y.style.display = "block";
+        } else {
+            y.style.display = "none";
+    }
+}
+function dispFunc3() {    
+    var z = document.getElementById("error");
+        if (z.style.display === "none") {
+            z.style.display = "block";
+        } else {
+            z.style.display = "none";
+    }
+} 
